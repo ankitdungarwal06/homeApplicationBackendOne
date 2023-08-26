322838a9e52a825cbfe20d9182f63bb44f2d8ffa
ankitdungarwal06 <ankitdgeek@gmail.com>
2023-08-26 18:12:28 +0530

    User model created, thymaleaf dependency added

---
+++ src/main/java/com/neelkanth/homeApplication/model/Users.java
@@ -0,0 +1,28 @@
+package com.neelkanth.homeApplication.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private Date dateOfBirth;
+	private String bloodGroup;
+	private String extra;
+}
